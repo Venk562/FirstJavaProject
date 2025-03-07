95191a720f8cde59b59e2c698110128df9507316
Venk562 <138963863+Venk562@users.noreply.github.com>
2025-03-07 06:13:18 +0530

    Create basic_java_program.java file

---
+++ basic_java_program.java
@@ -0,0 +1,16 @@
+class Basic
+  {
+    // public void firstProgram()
+    // {
+    //     system.out.println("Hello World");
+      
+    // }
+    public static void main(String arg[])
+    {
+       system.out.println("Hello World");
+    }
+    
+    
+  }
+ obj = new Basic();
+
